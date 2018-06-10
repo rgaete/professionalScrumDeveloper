c91bd72be24404d341712a331080208414216a6a
Fernando Ignacio Poblete Arrau <fernando.poblete@latam.com>
2018-06-09 20:42:54 -0400

    Adding initial data

--- initial/logbook/src/main/java/com/rgfp/psd/logbook/domain/Note.java
+++ initial/logbook/src/main/java/com/rgfp/psd/logbook/domain/Note.java
@@ -8,25 +8,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(name = "notes")
 public class Note {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String title;
     @CreationTimestamp
     private Timestamp timestamp;
     @Column(length=10000)
     private String content;
-    private LocalDateTime datetime;
 
     public String getSummary() {
-        return this.content.substring(0, 239);
+        return this.content.length() > 239 ? this.content.substring(0, 239) : this.content;
     }
 
 }
---
+++ initial/logbook/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO notes(title, content, timestamp) VALUES ('Supermercado', 'Compras semanales', NOW());
+INSERT INTO notes(title, content, timestamp) VALUES ('Giro cajero automatico', 'Para pagar el lavado del auto', NOW());
+INSERT INTO notes(title, content, timestamp) VALUES ('Futbolito', 'Perdimos y termine lesionado', NOW());
+INSERT INTO notes(title, content, timestamp) VALUES ('Traumatologo', 'Pedi hora por la lesion en el partido', NOW());
